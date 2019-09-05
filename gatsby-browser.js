67abdedd2630232f9da406fd59c7d7576b0265cc
Bernardo Srulzon <bernardo@getninjas.com.br>
2019-09-05 17:54:07 -0300

    Updating GA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
 const ReactGA = require('react-ga')
 
-ReactGA.initialize('UA-63104269-2')
+ReactGA.initialize('UA-25299114-21')
 ReactGA.set({
-  appName: 'Progression at Monzo'
+  appName: 'Progression at GetNinjas'
 })
 
 exports.onRouteUpdate = (state) => {
