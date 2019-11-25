d3297eae7d83e425a612b7fd1a6c06936d27c29c
Rodrigo Dlugokenski <rodrigo@qcx.com.br>
2019-11-25 13:27:48 -0300

    qc prog

--- src/components/layout.js
+++ src/components/layout.js
@@ -103,7 +103,7 @@ class Layout extends React.Component<Props, State> {
     return (
       <Contain>
         <Helmet>
-          <title>Progression at Monzo</title>
+          <title>Progression at Qconcursos.com</title>
         </Helmet>
         <SidebarRenderer
           data={data}
